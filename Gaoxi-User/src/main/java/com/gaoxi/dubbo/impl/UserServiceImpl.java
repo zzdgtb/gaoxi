5d7b8764e61aa69c18a51503286d2b5a8023091b
zzdgtb <552143501@qq.com>
2018-09-28 15:39:10 +0800

    张三修改密码

--- Gaoxi-User/src/main/java/com/gaoxi/dubbo/impl/UserServiceImpl.java
+++ Gaoxi-User/src/main/java/com/gaoxi/dubbo/impl/UserServiceImpl.java
@@ -14,7 +14,7 @@ public class UserServiceImpl implements UserService {
 	private static List<UserEntity> userlist = new ArrayList<UserEntity>();
 	
 	static{
-		userlist.add(new UserEntity("张三","123"));
+		userlist.add(new UserEntity("张三","1234"));
 		userlist.add(new UserEntity("李四","456"));
 	}
     @Override
